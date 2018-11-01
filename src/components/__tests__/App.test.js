2a94dd47b134c89b185edd28df078d8b15386a5c
Owais Khan <muhammad.owais@targetofs.com>
2018-11-01 23:49:38 +0500

    app component tests are completed

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme' // func: shallow renders *just* the given component and none of its children   
 
-import App from '../App'
+import App from 'components/App'
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
+
+let wrapper;
+beforeEach(() => { // func: beforeEach calls before every single test in current test page
+    wrapper = shallow(<App />)
+})
 
 it('shows a comment box', () => {
-    const div = document.createElement('div');
+    expect(wrapper.find(CommentBox).length).toEqual(1)
+})
 
-    ReactDOM.render(<App />, div);
-
-    expect(div.innerHTML).toContain('Comment Box');
-
-    ReactDOM.unmountComponentAtNode(div);
+it('shows a comment list', () => {
+    expect(wrapper.find(CommentList).length).toEqual(1)
 })
